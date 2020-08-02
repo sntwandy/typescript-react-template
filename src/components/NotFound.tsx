8d79115a4be8b084e8268d0e1a0251072b48f90d
sntwandy <sntwandy@gmail.com>
2020-08-02 10:40:53 -0400

    Last changes

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const NotFound = () : JSX.Element => {
   return(
     <div>
-      <h2>Error 404 Not Found!</h2>
+      <h2>Error 404 Not Found</h2>
     </div>
   );
 };

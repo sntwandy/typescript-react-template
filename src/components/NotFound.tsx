d6857cd4857b4dae23b9c3810ecaa079014319ed
sntwandy <sntwandy@gmail.com>
2020-08-02 10:25:03 -0400

    Adding last config, not found module

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const NotFound = () : JSX.Element => {
+  return(
+    <div>
+      <h2>Error 404 Not Found!</h2>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Components
 import Hello from '../components/Hello';
+import NotFound from '../components/NotFound';
 
 const App = () : JSX.Element => {
   return(
     <Router>
       <Switch>
         <Route exact path="/" component={Hello} />
+        <Route component={NotFound} />
       </Switch>
     </Router>
   );
